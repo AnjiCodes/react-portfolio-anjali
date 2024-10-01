6279252e6e297adf09f1c77007ce1a26d6c270a1
AnjiCodes <anjali.mahida25@gmail.com>
2024-10-01 01:03:48 -0400

    changed resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { FaLinkedin, FaGithub } from "react-icons/fa";
-import resume from "../assets/Anjali-Mahida_RESUME-LATEST.pdf";
+import resume from "../assets/Anjali-Resume.pdf";
 
 const Navbar = () => {
   return (
